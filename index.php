9cf65ea2b470d0260d9ad9f1695b7b6a8fa81085
MikkoLouise <louisemikko11@gmail.com>
2026-09-17 08:52:21 +0800

    Added table for students and books fetched from it30b_lab_db

--- index.php
+++ index.php
@@ -5,37 +5,171 @@
 $host = 'localhost';
 $db = 'it30b_lab_db';
 $user = 'root';
-$pass = "';
+$pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host; dbname=$db; charset=$charset";
+$dsn = "mysql:host=$host; dbname=$db; charset=$charset";
 
 $options = [
-    PDO::ATTR_ERR => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO:: ATTR_EMULATE_PREPARES => false,
-    ];
+ ];
 
     try{
         $pdo = new PDO($dsn,$user,$pass,$options);
-        echo 'connection successful';
     }catch(PDOException $e){
         die("Database connection failed" . $e->getMessage());
     }
 
+    // Session
+    session_start();
+
+    // Determine current section
+    $section = $_GET['section'] ?? 'students';
+
+    // Determnine CRUD operation
+    $action = $_GET['action'] ?? '';
 
 
+    // Fetch Students
+    if($section === 'students'){
+        $stmt = $pdo->query("
+        SELECT *
+        FROM students
+        ORDER BY student_id DESC;
+        ");
+
+        $students = $stmt->fetchAll();
+
+    }
+
+    if ($section === 'books') {
+        $stmt = $pdo->query(" 
+        SELECT *
+         FROM books 
+         ORDER BY book_id DESC ");
+
+          $books = $stmt->fetchAll(); } ?>
 
 
-    ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Library System</title>
     </head>
     <body>
+        <h1>Simple Library System</h1>
+        <nav>
+            <a href="index.php?section=students">Students</a>
+            <a href="index.php?section=books">Books</a>
+            <a href="index.php?section=borrow">Borrow</a>
+        </nav>
+        <hr>
+        <?php if($section === 'students'): ?>
+            <h1>Students</h1>
+        <table>
+         <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Created At</th>
+                <th>Actions</th>
+
+            </tr>
+        <thead>
+        <tbody>
+
+            <?php foreach($students as $student): ?>
+                <tr>
+                    <td>
+                    <?=htmlspecialchars($student['student_id']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($student['student_first_name']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($student['student_last_name']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($student['student_course']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($student['student_created_at']) ?>
+                    </td>
+
+                    <td>
+                        <a>Edit</a>
+
+                        <a>Delete</a>
+
+                    </td>
+            </tr>
+            <?php endforeach?>
+            </tbody>
+            </table>
+
+
+
+        <?php endif; ?>
+
+         <?php if($section === 'books'): ?>
+            <h1>Books</h1>
+            <table>
+         <thead>
+            <tr>
+                <th>ID</th>
+                <th>Book Title</th>
+                <th>Author</th>
+                <th>Category</th>
+                <th>Book Created At</th>
+                <th>Actions</th>
+
+            </tr>
+        <thead>
+        <tbody>
+
+            <?php foreach($books as $book): ?>
+                <tr>
+                    <td>
+                    <?=htmlspecialchars($book['book_id']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($book['book_title']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($book['book_author']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($book['book_category']) ?>
+                    </td>
+                    <td>
+                    <?=htmlspecialchars($book['book_created_at']) ?>
+                    </td>
+
+                    <td>
+                        <a>Edit</a>
+
+                        <a>Delete</a>
+
+                    </td>
+            </tr>
+            <?php endforeach?>
+            </tbody>
+            </table>
+
+        <?php endif; ?>
+
+         <?php if($section === 'borrow'): ?>
+            <h1>Borrow</h1>
+        <?php endif; ?>
+
         
+
     </body>
     </html>
+
